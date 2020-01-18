e19e9c087cfcbae22f223a0777546afeea2d92ce
Svanhild Sleire <svanesdesign@gmail.com>
2020-01-18 13:59:29 +0100

    Create level-2.js

---
+++ level-2.js
@@ -0,0 +1,16 @@
+// question 1
+
+for (var i = 15; i <= 25; i++) {
+  if (i % 2 === 0) console.log(i);
+}
+
+// question 2
+
+function innerFunction() {
+  console.log("I am a function");
+}
+function outerFunction(argument) {
+  argument();
+}
+
+outerFunction(innerFunction);
